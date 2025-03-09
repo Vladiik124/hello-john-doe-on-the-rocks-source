4a906684eca8463bebafa73f2f567cd4625c0dd2
vladiik124 <vladimirkotliar8@gmail.com>
2025-03-10 01:48:16 +0200

    removed the slop

--- modsOnTheRocks/devilish/events/infryzoom.lua
+++
@@ -1,8 +0,0 @@
-function onEvent(eventName, value1, value2) 
-    if eventName == 'infryzoom' then
-        cancelTween("asf")
-        doTweenZoom('asf', 'camGame', value1, value2, 'cubeOut')
-        setProperty('defaultCamZoom', value1)
-        
-    end
-end
